db903d0528aea36951e3343367f61f5917b978c7
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2022-05-22 13:27:21 +0200

    Dobrze jest

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__link" href="${image.original}"> <img class= "gallery__image" src="${image.preview}" data-source=${image.original}
+ alt="${image.description}"></a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
+
+gallery.onclick = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let source = e.target.dataset.source;
+  basicLightbox.create(`<img src="${source}">`).show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}"> <img class= "gallery__image" src="${image.preview}" 
+ alt="${image.description}"></a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+})
